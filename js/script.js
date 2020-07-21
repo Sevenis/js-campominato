fb1ee3f036c25eb97395714699d93175c8376b17
Pietro Sette <pietrosette@gmail.com>
2020-07-21 12:31:31 +0200

    rev

--- js/script.js
+++ js/script.js
@@ -16,48 +16,52 @@ var listaNumeriScelti = [];
 var numeroRandom;
 var numeroScelto;
 var controllo;
-var range;
 
-// Si seleziona la difficoltà del gioco
-var difficolta = parseInt(prompt('Scegli il grado di difficoltà del gioco da 0 a 2'));
+//Si seleziona la difficoltà del gioco
+do {
+    var difficolta = parseInt(prompt('Scegli il grado di difficoltà del gioco da 0 a 2'));
+    switch (difficolta){
+        case 0:
+            var range = 100;
+            break;
+        case 1:
+            var range = 80;
+            break;
+        case 2:
+            var range = 50;
+            break;
+        default:
+            var range = 100;
+    }
+} while (difficolta < 0 || difficolta > 2);
 
-switch (difficolta){
-    case 0:
-    range = 100;
-    case 1:
-    range = 80;
-    case 2:
-    range = 50;
-}
-
-//si riempie un array di numeri random ognuno diverso dall'altro.
+//Si riempie un array di numeri random ognuno diverso dall'altro.
 while (listaNumeriRandom.length < 16) {
-    do {
-        numeroRandom = (randomNumber(1,range));
-        controllo = checkElement(listaNumeriRandom, numeroRandom);
-    } while(controllo == true)
-    listaNumeriRandom.push(numeroRandom);
+    numeroRandom = (randomNumber(1,range));
+    controllo = checkElement(listaNumeriRandom, numeroRandom);
+    if(controllo == false){
+        listaNumeriRandom.push(numeroRandom);
+    }
 }
 
-//ordina gli elementi dell'array random.
-listaNumeriRandom.sort();
-console.log(listaNumeriRandom);
-
-//Logica gioco
+//Logica del gioco
 do {
     //Chiede un numero fino alla sconfitta oppure al raggiungimento del valore max
     //settato dalla difficoltà del gioco
     if (listaNumeriScelti.length < (range - 16)){
         numeroScelto = parseInt(prompt('Scegli un numero!'));
         controllo = checkElement(listaNumeriRandom, numeroScelto);
-        if(controllo==true){
+        if(controllo == true){
             alert('Hai beccato una mina! Hai perso!');
+        } else if (numeroScelto < 0 || numeroScelto > range) {
+            alert('Devi scegliere un numero positivo minore di: ' + range);
+            controllo = false;
         } else {
             // controllo se numero inserito è gia' inserito
             controllo = checkElement(listaNumeriScelti, numeroScelto);
-            if(controllo==true){
+            if(controllo == true){
                 alert('Numero già scelto, riprova!');
-                controllo = false;
+                controllo = false; // reitera il ciclo fino a quando non si inserisce un numero non inserito
             } else {
                 //inserisce valore nella lista dei numeri scelti (punti)
                 listaNumeriScelti.push(numeroScelto);
@@ -66,12 +70,11 @@ do {
     } else {
         //Caso: array numeri scelti riempito tutto (trovate tutte le mine). Vittoria
         alert('Complimenti, hai vinto!!!');
-        controllo=true;
+        controllo = true;
     }
-} while (controllo==false);
+} while (controllo == false);
 
 //Stampa a video
-
 document.getElementById('campo-minato').innerHTML = 'Hai totalizzato: ' + listaNumeriScelti.length + ' punti!';
 document.getElementById('lista-numeri-random').innerHTML = "Ecco le mine presenti: " + listaNumeriRandom;
 document.getElementById('lista-numeri-scelti').innerHTML = "Ecco i tuoi punti: " + listaNumeriScelti;
@@ -85,13 +88,14 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-//dato un elemento e un array, controlla che l'elemento sia presente nell'array
+//Dato un elemento e un array, controlla che l'elemento sia presente nell'array
 //restituisce true se l'elemento è presente nell'array.
 function checkElement (array, element){
     var check = false;
     for(var i = 0; i < array.length; i ++){
         if (element == array[i]){
             check = true;
+            return check;
         }
     }
     return check;
